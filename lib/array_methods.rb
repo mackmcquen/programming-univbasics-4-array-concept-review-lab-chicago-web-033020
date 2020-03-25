4aea6e5d52a688d2e9f5cef313d14bc8654164ab
Mack McQuen <mackmcquen@comcast.net>
2020-03-25 04:07:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,9 +8,15 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  checker = array[0]
+  array.length.times { |index|
+    if array[index] > checker
+      checker = array[index]
+    end
+  }
+  return checker
 end
 
 def find_min_value(array)
-  # Add your solution here
+  
 end
