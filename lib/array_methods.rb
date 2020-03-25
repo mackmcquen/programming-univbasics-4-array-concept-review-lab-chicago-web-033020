f38fd706c030cec52b7b81af8d2edfa691c10786
Mack McQuen <mackmcquen@comcast.net>
2020-03-25 04:07:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,5 +18,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  
+  checker = array[0]
+  array.length.times { |index|
+    if array[index] < checker
+      checker = array[index]
+    end
+  }
+  return checker
+
 end
